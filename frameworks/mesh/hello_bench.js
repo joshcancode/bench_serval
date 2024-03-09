32c4115e0e0ab14c103faa2c64ff92c483d65e8c
joshcancode <joshuafountain1@gmail.com>
2024-03-09 11:47:40 -0500

    Fix Mesh

--- frameworks/mesh/hello_bench.js
+++ frameworks/mesh/hello_bench.js
@@ -1,6 +1,5 @@
-import { App } from '@ionited/mesh';
-
-const app = new App();
+const Mesh = require("@ionited/mesh");
+const app = new Mesh.App();
 
 app
 	.get('/', (req, res) => res.send("Hello, Bench!"))
